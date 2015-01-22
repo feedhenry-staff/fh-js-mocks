40e1a81e922803b23a88a071fe4e192e01b55a73
Evan Shortiss <evan.shortiss@feedhenry.com>
2015-01-22 08:44:13 -0500

    Update build

--- dist/fh-mocks.js
+++ dist/fh-mocks.js
@@ -2,37 +2,185 @@
 'use strict';
 
 /**
- * A handler class for managing $fh.act mock responses
+ * A handler class for managing $fh.api mock responses
  * @constructor
- * @param {Object} args   The args provided to $fh.act
+ * @param {Object} args   The args provided to $fh.api call
  */
-function MockActRequest (args, success, fail) {
+function MockApiCall (args, success, fail) {
   this._args = args;
   this._fail = fail;
   this._success = success;
   this._responded = false;
 }
-module.exports = MockActRequest;
+module.exports = MockApiCall;
 
-MockActRequest.prototype.getArgs = function () {
+MockApiCall.prototype.getArgs = function () {
   return this._args;
 };
 
-MockActRequest.prototype.fail = function () {
+MockApiCall.prototype.fail = function () {
   this._fail.apply(this, Array.prototype.slice.call(arguments));
   this._responded = true;
 };
 
-MockActRequest.prototype.success = function () {
+MockApiCall.prototype.success = function () {
   this._success.apply(this, Array.prototype.slice.call(arguments));
   this._responded = true;
 };
 
-MockActRequest.prototype.hasReceivedResponse = function () {
+MockApiCall.prototype.hasReceivedResponse = function () {
   return this._responded;
 };
 
 },{}],2:[function(require,module,exports){
+'use strict';
+
+
+var MockApiResponder = require('./MockApiResponder')
+  , MockApiCall = require('./MockApiCall');
+
+
+module.exports = function getMockApiInstance () {
+  var expectations = []
+    , requests = [];
+
+  /**
+   * An api mock function that mimics a $fh.api API
+   * @retuns  {Function}
+   */
+  function MockApiInstance(opts, success, fail) {
+    requests.push(new MockApiCall(opts, success, fail));
+  }
+
+  /**
+   * Add a request
+   * @param   {Object}    opts
+   * @param   {Function}  success
+   * @param   {Function}  fail
+   * @retuns  {MockApiResponder}
+   */
+  MockApiInstance.makeRequest = function (opts, success, fail) {
+    requests.push(new MockApiCall(opts, success, fail));
+  };
+
+  /**
+   * Setup an api expectation. Simply provide the parameters to expect.
+   * @param   {Object}          opts
+   * @returns {MockApiResponder}
+   */
+  MockApiInstance.expect = function (opts) {
+    var h = new MockApiResponder(opts);
+
+    expectations.push(h);
+
+    return h;
+  };
+
+  /**
+   * Represent the number of requests awaiting a response.
+   * @returns {Number}
+   */
+  MockApiInstance.queLength = function () {
+    return requests.length;
+  };
+
+
+  /**
+   * Remove all expectations that were defined.
+   */
+  MockApiInstance.resetExpectations = function () {
+    expectations = [];
+    requests = [];
+  };
+
+
+  /**
+   * Check that all requests were flushed successfully.
+   */
+  MockApiInstance.verifyNoOutstandingRequest = function () {
+    if (requests.length) {
+      throw new Error('api has unflushed requests ' + requests.join(', '));
+    }
+  };
+
+
+  /**
+   * Check that all expectations have been satisfied.
+   */
+  MockApiInstance.verifyNoOutstandingExpectation = function () {
+    if (expectations.length) {
+      throw new Error('api has unsatisfied expectations ' +
+        expectations.join(', '));
+    }
+  };
+
+
+  /**
+   * Get the handler for a given request.
+   * @param   {Object} req
+   * @returns {MockApiResponder}
+   */
+  MockApiInstance.getExpectationForRequest = function (req) {
+    for (var i = 0; i < expectations.length; i++) {
+      if (expectations[i].argsMatch(req)) {
+        return expectations[i];
+      }
+    }
+
+    return null;
+  };
+
+
+  /**
+   * Flush all pending requests for resolution.
+   * Requests are responded to on a first in first out basis.
+   * @param {Number} count - How many queued requests to responed to.
+   */
+  MockApiInstance.flush = function (count, force) {
+    var req = null
+      , expectation = null;
+
+    if (typeof count === 'boolean') {
+      force = count;
+      count = requests.length;
+    } else {
+      count = count || requests.length;
+    }
+
+    // Loop requests and respond where possible
+    for (var i = 0; i < count; i++) {
+      req = requests[i];
+      expectation = this.getExpectationForRequest(req);
+
+      if (expectation) {
+        expectation.respond(req);
+      }
+    }
+
+    // Remove requests that have received a response
+    requests = requests.filter(function (r) {
+      if (force) {
+        return false;
+      } else {
+        return !r.hasReceivedResponse();
+      }
+    });
+
+    // Remove expectations that responded to a request
+    expectations = expectations.filter(function (e) {
+      if (force) {
+        return false;
+      } else {
+        return !e.hasResponded();
+      }
+    });
+  };
+
+
+  return MockApiInstance;
+};
+
+},{"./MockApiCall":1,"./MockApiResponder":3}],3:[function(require,module,exports){
 'use strict';
 
 /**
@@ -40,22 +188,22 @@ MockActRequest.prototype.hasReceivedResponse = function () {
  * @constructor
  * @param {Object} args - The args provided to $fh.{METHOD}
  */
-function MockHandler (args) {
+function MockApiResponder (args) {
   this._expectedArgs = args;
   this._response = {};
   this._errBack = false;
   this._responded = false;
 }
-module.exports = MockHandler;
+module.exports = MockApiResponder;
 
 
 /**
  * Get the response that should be returned by this handler.
  * @param   {Mixed} err - The err parameter you'd send to the cloud callback.
  * @param   {Mixed} res - The res parameter you'd send to the cloud callback.
- * @returns {MockHandler}
+ * @returns {MockApiResponder}
  */
-MockHandler.prototype.setResponse = function(err, res) {
+MockApiResponder.prototype.setResponse = function(err, res) {
   if (err) {
     this._errBack = true;
     this._response = err;
@@ -73,7 +221,7 @@ MockHandler.prototype.setResponse = function(err, res) {
  * setResponse
  * @param {MockActRequest} req
  */
-MockHandler.prototype.respond = function (req) {
+MockApiResponder.prototype.respond = function (req) {
   if (this._errBack) {
     // TODO: Update to mimic the object adnd messages returned from the SDK
     req.fail(this._response);
@@ -89,7 +237,7 @@ MockHandler.prototype.respond = function (req) {
  * Check has this request been responded to.
  * @returns {Boolean}
  */
-MockHandler.prototype.hasResponded = function() {
+MockApiResponder.prototype.hasResponded = function() {
   return this._responded;
 };
 
@@ -98,7 +246,7 @@ MockHandler.prototype.hasResponded = function() {
  * Get the args expected by this handler.
  * @return  {Mixed}
  */
-MockHandler.prototype.getExpectedArgs = function() {
+MockApiResponder.prototype.getExpectedArgs = function() {
   return this._expectedArgs;
 };
 
@@ -108,174 +256,91 @@ MockHandler.prototype.getExpectedArgs = function() {
  * @param {MockActRequest}  req
  * @param {Function}        callback
  */
-MockHandler.prototype.argsMatch = function (req) {
+MockApiResponder.prototype.argsMatch = function (req) {
   var argsStr = JSON.stringify(req.getArgs())
     , expectedStr = JSON.stringify(this.getExpectedArgs());
 
   return (expectedStr === argsStr);
 };
 
-},{}],3:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 'use strict';
 
-
-var MockHandler = require('./MockHandler')
-  , MockActRequest = require('./MockActRequest');
-
-
-/**
- * Handlers for expected $fh.act calls.
- * @private
- */
-var expectations = []
-  , requests = [];
+var MockApiInstance = require('./MockApiInstance')
+  , xtend = require('xtend')
+  , originalApiFns = {};
 
 
-/**
- * Get the handler for a given request.
- * @param   {Object} req
- * @returns {MockHandler}
- */
-function getExpectationForRequest (req) {
-  for (var i = 0; i < expectations.length; i++) {
-    if (expectations[i].argsMatch(req)) {
-      return expectations[i];
-    }
-  }
-
-  return null;
-}
-
-
-/**
- * An Act mock function that mimics the $fh.act API
- * @param   {Object}    opts
- * @param   {Function}  success
- * @param   {Function}  fail
- * @retuns  {MockHandler}
- */
-var MockAct = module.exports = function MockAct(opts, success, fail) {
-  requests.push(new MockActRequest(opts, success, fail));
-};
-
-
-/**
- * Setup an Act expectation. Simply provide the parameters to expect.
- * @param   {Object}          opts
- * @returns {MockHandler}
- */
-MockAct.expect = function (opts) {
-  var h = new MockHandler(opts);
-
-  expectations.push(h);
-
-  return h;
-};
-
-/**
- * Represent the number of requests awaiting a response.
- * @returns {Number}
- */
-MockAct.queLength = function () {
-  return requests.length;
-};
-
-
-/**
- * Remove all expectations that were defined.
- */
-MockAct.resetExpectations = function () {
-  expectations = [];
-  requests = [];
-};
-
-
-/**
- * Check that all requests were flushed successfully.
- */
-MockAct.verifyNoOutstandingRequest = function () {
-  if (requests.length) {
-    throw new Error('Act has unflushed requests ' + requests.join(', '));
-  }
-};
-
-
-/**
- * Check that all expectations have been satisfied.
- */
-MockAct.verifyNoOutstandingExpectation = function () {
-  if (expectations.length) {
-    throw new Error('Act has unsatisfied expectations ' +
-      expectations.join(', '));
-  }
-};
-
-
-/**
- * Flush all pending requests for resolution.
- * Requests are responded to on a first in first out basis.
- * @param {Number} count - How many queued requests to responed to.
- */
-MockAct.flush = function(count, force) {
-  var req = null
-    , expectation = null;
-
-  if (typeof count === 'boolean') {
-    force = count;
-    count = requests.length;
-  } else {
-    count = count || requests.length;
-  }
-
-  // Loop requests and respond where possible
-  for (var i = 0; i < count; i++) {
-    req = requests[i];
-    expectation = getExpectationForRequest(req);
-
-    if (expectation) {
-      expectation.respond(req);
-    }
-  }
-
-  // Remove requests that have received a response
-  requests = requests.filter(function (r) {
-    if (force) {
-      return false;
-    } else {
-      return !r.hasReceivedResponse();
-    }
-  });
-
-  // Remove expectations that responded to a request
-  expectations = expectations.filter(function (e) {
-    if (force) {
-      return false;
-    } else {
-      return !e.hasResponded();
-    }
-  });
-};
-
-},{"./MockActRequest":1,"./MockHandler":2}],4:[function(require,module,exports){
-'use strict';
-
+// Allow use as a standard node.js module / with browserify
 module.exports = {
-  // Stub interfaces are exposed here
-  act: require('./act')
+  createApiShim: createApiShim,
+  restoreOriginalApi: restoreOriginalApi
 };
 
-// Override the existing $fh implementation on the window object if required
+
+// Ensure window.$fh is defined
 if (typeof window !== 'undefined') {
   if (!window.$fh) {
     window.$fh = {};
   }
 
-  // Override $fh APIs
-  for (var i in module.exports) {
-    if (module.exports.hasOwnProperty(i)) {
-      window.$fh[i] = module.exports[i];
-    }
+  // Add the mock's extensions to the FH SDK
+  window.$fh = xtend(window.$fh, module.exports);
+}
+
+/**
+ * Create a shim for the given API and override the one on window.$fh
+ * @param  {String}   apiName     The api you want to shim e.g "cloud"
+ * @param  {Boolean}  noOverride  If true window.$fh api will not be replaced.
+ *                                You'll probably rarely/never set this to true
+ * @return {MockApiInstance}  A mock API instance to play with
+ */
+function createApiShim (apiName, noOverride) {
+  var shim = new MockApiInstance();
+
+  // Store the original function so it can be restored
+  if (typeof window !== 'undefined') {
+    originalApiFns[apiName] = window.$fh[apiName];
+  }
+
+  // Override the existing version?
+  if (!noOverride && typeof window !== 'undefined') {
+    window.$fh[apiName] = shim;
+  }
+
+  // Bind the shim to the exports object
+  return module.exports[apiName] = shim;
+}
+
+
+/**
+ * Restore an original $fh.api that was overwritten by createApiShim.
+ * Only works if window.$fh is defined.
+ * @param  {String} apiName The API whose original function you to restore
+ */
+function restoreOriginalApi (apiName) {
+  if (typeof window !== 'undefined') {
+    window.$fh = originalApiFns[apiName];
   }
 }
 
-},{"./act":3}]},{},[4]);
+},{"./MockApiInstance":2,"xtend":5}],5:[function(require,module,exports){
+module.exports = extend
+
+function extend() {
+    var target = {}
+
+    for (var i = 0; i < arguments.length; i++) {
+        var source = arguments[i]
+
+        for (var key in source) {
+            if (source.hasOwnProperty(key)) {
+                target[key] = source[key]
+            }
+        }
+    }
+
+    return target
+}
+
+},{}]},{},[4]);
